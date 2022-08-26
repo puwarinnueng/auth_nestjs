1c9f96dad749681cf9ca28c5e5611102836fa7b2
Puwarin Maneejansuk <puwarin@mfec.co.th>
2022-08-26 13:22:41 +0700

    update jwt web token

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,8 +1,13 @@
-import { Controller, Post, Request, UseGuards } from '@nestjs/common';
+import { AuthService } from './auth.service';
+import { Controller, Get, Post, Request, UseGuards } from '@nestjs/common';
 import { LocalAuthGuard } from './local/local-auth.guards';
+import { JwtAuthGuard } from './jwt/jwt-auth.guards';
 
 @Controller('auth')
 export class AuthController {
+
+    constructor(private authService: AuthService){}
+    
 
     //useguard ดรียก stratigy เพื่อตรวจสอบ ทุกครั้งที่ยิง requestมา
     @UseGuards(LocalAuthGuard)
@@ -10,7 +15,13 @@ export class AuthController {
     signIn(
         @Request() req: any
     ): Promise<any> {
-        return req.user
+        return this.authService.signIn(req.user)
+    }
 
+
+    @UseGuards(JwtAuthGuard)
+    @Get('profile')
+    getProfile(@Request() req:any): Promise<any>{
+        return req.user
     }
 }
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,20 +1,32 @@
 import { Injectable } from '@nestjs/common';
 import { UserService } from 'src/user/user.service';
 import * as bcrypt from 'bcrypt'
+import { JwtService } from '@nestjs/jwt';
 
 @Injectable()
 export class AuthService {
-   
-    constructor(private userService: UserService){}
+
+    constructor(
+        private userService: UserService,
+        private jwtService: JwtService,
+    ) { }
 
 
-    async validateUser(username:string , password:string): Promise<any>{
-         const user = await this.userService.findOneUser(username)
-         if(user && await bcrypt.compare(password , user.password)){
+    async validateUser(username: string, password: string): Promise<any> {
+        const user = await this.userService.findOneUser(username)
+        if (user && await bcrypt.compare(password, user.password)) {
             const { password, ...result } = user
             return result
-         }
+        }
 
-         return null
+        return null
+    }
+
+    async signIn(user: any) {
+        const payload = { username: user.username, sub: user.id }
+
+        return {
+            accessToken: this.jwtService.sign(payload)
+        }
     }
 }
